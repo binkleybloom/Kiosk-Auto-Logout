d5563d65e0b98eda7532f13d2220c07ea1f3f80f
binkleybloom <taschutt@syr.edu>
2014-07-15 16:18:41 -0400

    Initial commit.

---
+++ usr-local/CheckRunningProcess.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+"""
+This script watches a specific process (second item in "cmd"), and logs the user out when the process exits.
+Tim Schutt, taschutt@syr.edu
+July, 2014
+"""
+
+import os, subprocess, sys, time
+
+username = "test" # set to the shortname of the kiosk user
+appname = "Microsoft Remote Desktop" # set to the name of the app you wish to monitor
+
+def processrunning():
+  cmd = ["/usr/bin/pgrep", appname]
+  proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  (output, error_output) = proc.communicate()
+  return output
+
+def main():
+  if processrunning():
+    time.sleep(1)
+    main()
+  else:
+    os.system("/usr/bin/touch /Users/Shared/.com.github.binkleybloom.logoutnow")
+    sys.exit(0)
+
+if __name__ == "__main__":
+  if os.getlogin() == username:
+    time.sleep(10) # waits 10 seconds for application to launch before checking
+    main()
+  else:
+    print "Whoops! Wrong user."
+    sys.exit(0)
---
+++ usr-local/logout.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm /Users/Shared/.com.github.binkleybloom.logoutnow
+/usr/bin/killall loginwindow
+exit 0
